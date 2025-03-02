d1971a3f3e3c58b747a2af7ee725d37ea6963867
David Enault de Courval <dedcourval@outlook.com>
2025-03-02 14:18:25 -0500

    feat: added namespace utility function

--- www/basic-p2p/js/main.js
+++ www/basic-p2p/js/main.js
@@ -82,3 +82,15 @@ function handleCallButton(event) {
 /**
  *  Utility Functions
  */
+
+function prepareNamespace(hash, set_location) {
+  let ns = hash.replace(/^#/, ""); // remove # from the hash
+  if (/^[0-9]{7}$/.test(ns)) {
+    console.log("Checked existing namespace", ns);
+    return ns;
+  }
+  ns = Math.random().toString().substring(2, 9);
+  console.log("Created new namespace", ns);
+  if (set_location) window.location.hash = ns;
+  return ns;
+}
